c88d7da1a97288a37af2e8bcdc7cf6ddedc7a853
Sachin <abcd@abc.com>
2016-10-15 15:54:54 +0530

    'assignment2'

--- Module2-solution/assignment2-starter-code/app.js
+++ Module2-solution/assignment2-starter-code/app.js
@@ -1,32 +1,42 @@
-(function(){
+(function()
+{
   'use strict';
+  var ListOfitems=[
+  {name:"Coookie",quantity:"2"},
+  {name:"Cake",quantity:"2"},
+  {name:"Sugar",quantity:"12"},
+  {name:"Curd",quantity:"5"},
+  {name:"Cinnamaon",qunatity:"9"}];
 
   angular.module('Shopping',[])
-  .controller('ShoppingListController',ShoppingListController)
-  .controller('BuyingController',BuyingController)
-  .controller('BoughtController',BoughtController);
+  .controller('ShoppingListController',ShoppingListController);
 
 
-  ShoppingListController.$inject=['this']
-  function ShoppingListController(this){
-    var item= {
-      name="";
-      quantity="";
+  ShoppingListController.$inject=['$scope'];
+  function ShoppingListController($scope){
+   $scope.name="";
+   $scope.quantity="";
+
+    $scope.itemList=ListOfitems;
+    $scope.itemList2=[];
+
+    $scope.addItemToList=function(name,quantity){
+      console.log(name,qunatity);
+      var item= {name,quantity};
+      $scope.itemList.push(item);
+
+    }
+    $scope.removeItem=function(name,quantity){
+      var item={name,qunatity};
+      $scope.itemList2.push(item);
+      $scope.itemList.remove(item);
     }
 
-    var ListOfitems=[];
-    var ctrl=this;
 
-    ctrl.addItemToList
-
-
+  };
+  //BuyingController.$inject=[]
+  //BoughtController.$inject=[]
 
 
 
-  }
-  BuyingController.$inject=[]
-  BoughtController.$inject=[]
-
-
-
-})
+});
